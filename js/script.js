3f7a6b1f3a2529c0be911144198a290acfadfa51
SavyonMalko <124379798+SavyonMalko@users.noreply.github.com>
2023-04-23 00:21:16 +0000

    done for now

--- js/script.js
+++ js/script.js
@@ -1,48 +1,24 @@
+function calculateCost() {
+  const timbitsPack = document.getElementById("timbitsPack").value;
+  const toppings = document.querySelectorAll('input[name="toppings"]:checked');
+  const coffeeSize = document.getElementById("coffeeSize").value;
 
-function calculateOrder() {
-    // Get the user input values
-    let pack = parseInt(document.getElementById("pack").value);
-    let toppings = document.getElementsByName("toppings");
-    let coffee = parseFloat(document.getElementById("coffee").value);
+  let toppingsCost = 0;
+  toppings.forEach((topping) => {
+    const toppingCost = parseFloat(topping.value);
+    toppingsCost += toppingCost;
+  });
 
-    // Calculate the Timbits cost
-    let timbitsCost = 0;
-    if (pack === 10) {
-        timbitsCost = 3.19;
-    } else if (pack === 20) {
-        timbitsCost = 4.99;
-    } else if (pack === 50) {
-        timbitsCost = 9.99;
-    }
-    timbitsCost += calculateToppingsCost(toppings);
+  const timbitsCost = parseFloat(timbitsPack) * (toppingsCost + 0.01);
+  const coffeeCost = parseFloat(coffeeSize);
+  const subtotal = timbitsCost + coffeeCost;
+  const tax = 0.13 * subtotal;
+  const total = subtotal + tax;
 
-    // Calculate the coffee cost
-    let coffeeCost = coffee;
+  const orderSummary = `Your order: ${timbitsPack} Timbits with ${toppings.length} toppings and a ${coffeeSize} coffee`;
+  const subtotalDisplay = `Subtotal: $${subtotal.toFixed(2)}`;
+  const totalDisplay = `Total (including tax): $${total.toFixed(2)}`;
 
-    // Calculate the subtotal and tax
-    let subtotal = timbitsCost + coffeeCost;
-    let tax = subtotal * 0.13;
-    let total = subtotal + tax;
-
-    // Display the order summary
-    let orderSummary = "You ordered " + pack + " Timbits with toppings and a coffee size for $" + total.toFixed(2);
-    document.getElementById("orderSummary").innerHTML = orderSummary;
-}
-
-function calculateToppingsCost(toppings) {
-    let toppingsCost = 0;
-    for (let i = 0; i < toppings.length; i++) {
-        if (toppings[i].checked) {
-            if (toppings[i].value === "chocolate") {
-                toppingsCost += 0.02;
-            } else if (toppings[i].value === "sprinkles") {
-                toppingsCost += 0.01;
-            } else if (toppings[i].value === "jam") {
-                toppingsCost += 0.02;
-            } else if (toppings[i].value === "caramel") {
-                toppingsCost += 0.01;
-            }
-        }
-    }
-    return toppingsCost;
+  document.getElementById("orderSummary").textContent = orderSummary;
+  document.getElementById("orderTotal").textContent = `${subtotalDisplay}\n${totalDisplay}`;
 }
