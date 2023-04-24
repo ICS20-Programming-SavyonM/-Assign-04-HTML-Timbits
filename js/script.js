6f0d852a96b510ca3693ccdfe7abf2c56de55f7a
SavyonMalko <124379798+SavyonMalko@users.noreply.github.com>
2023-04-24 15:11:39 +0000

    Table done

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
+// Copyright (c) 2023 Savyon All rights reserved
+//
+// Created by: Savyon
+// Created on: April 2023
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function displays what the user has ordered and the total cost
+ */
 function calculateTotal() {
 	// Get the selected values from the dropdown menus
-	var timbits = document.getElementById("timbits").value;
-	var toppings = document.getElementById("toppings").value;
-	var coffee = document.getElementById("coffee").value;
+	let timbits = document.getElementById("timbits").value;
+	let toppings = document.getElementById("toppings").value;
+	let coffee = document.getElementById("coffee").value;
 
 	// Calculate the subtotal and total cost
-	var subtotal = (timbits * 1) + (timbits * toppings) + (coffee * 1);
-	var total = subtotal * 1.13;
+	let subtotal = (timbits * 1) + (timbits * toppings) + (coffee * 1);
+	let total = subtotal * 1.13;
 
-	// Update the display
+	// Update the display each time the 
 	document.getElementById("subtotal").innerHTML = "$" + subtotal.toFixed(2);
 	document.getElementById("total").innerHTML = "$" + total.toFixed(2);
 }
 
 function submitOrder() {
 	// Get the selected values from the dropdown menus
-	var timbits = document.getElementById("timbits").value;
-	var toppings = document.getElementById("toppings").options[document.getElementById("toppings").selectedIndex].text;
-	var coffee = document.getElementById("coffee").options[document.getElementById("coffee").selectedIndex].text;
+	let timbits = document.getElementById("timbits").value;
+	let toppings = document.getElementById("toppings").options[document.getElementById("toppings").selectedIndex].text;
+	let coffee = document.getElementById("coffee").options[document.getElementById("coffee").selectedIndex].text;
 
 	// Create a summary of the order
-	var summary = "<li>" + timbits + " Timbits with " + toppings + "</li>";
+	let summary = "<li>" + timbits + " Timbits with " + toppings + "</li>";
 	summary += "<li>" + coffee + "</li>";
 
 	// Update the display
